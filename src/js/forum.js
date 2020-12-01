1589c00034d7fa460a3cb53ae388ec7709f19992
Julien Blatecky <julien.blatecky@hey.com>
2020-12-01 11:20:51 +0100

    Send messages

--- src/js/forum.js
+++ src/js/forum.js
@@ -23,18 +23,65 @@ function displayMessages(messages) {
 }
 
 async function refreshMessages() {
-  // GET https://ensmn.herokuapp.com/messages
-  const messages = await ky.get("https://ensmn.herokuapp.com/messages").json();
-  displayMessages(messages);
+  let pageIndex = 0;
+  let hasMessages = true;
+  let messages = [];
+
+  while (hasMessages) {
+    // GET https://ensmn.herokuapp.com/messages
+    // eslint-disable-next-line
+    const pageMessages = await ky
+      .get(`https://ensmn.herokuapp.com/messages?page=${pageIndex}`)
+      .json();
+
+    hasMessages = pageMessages.length > 0;
+    pageIndex += 1;
+    messages = messages.concat(pageMessages);
+  }
+
+  displayMessages(messages.reverse());
 }
 
 refreshMessages();
 
 setInterval(() => {
   refreshMessages();
-}, 1000);
+}, 5000);
 
-// function sendMessage(message) {
-//   // POST https://ensmn.herokuapp.com/messages (username, message)
-//   // After success, getMessages()
-// }
+function sendMessage(message) {
+  // POST https://ensmn.herokuapp.com/messages (username, message)
+  ky.post("https://ensmn.herokuapp.com/messages", { json: message }).then(() =>
+    refreshMessages()
+  );
+}
+
+// Listen "submit" event from form#message-form in body
+$("body").on("submit", "#message-form", (event) => {
+  // Stop default form submission
+  event.preventDefault();
+
+  // Get <form id="message-form">
+  const $form = $("#message-form");
+
+  // Retrieve data from form
+  const data = $form.serializeArray();
+
+  const messageData = {};
+  data.forEach(({ name, value }) => {
+    messageData[name] = value;
+  });
+
+  // Validate data
+  if (
+    messageData.username == null ||
+    messageData.username.length === 0 ||
+    messageData.message == null ||
+    messageData.message.length === 0
+  ) {
+    return;
+  }
+
+  sendMessage(messageData);
+
+  $("#message").val("");
+});
